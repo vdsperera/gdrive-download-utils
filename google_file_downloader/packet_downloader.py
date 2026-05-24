526b5c2a8d5554564e85a852b1f23c5d3c2375a1
Vidumini Sulochana <kadvsperera@gmail.com>
2026-05-24 17:14:57 +0530

    feat: make it possible to use custom file name pattern for saving file name

--- google_file_downloader/packet_downloader.py
+++ google_file_downloader/packet_downloader.py
@@ -12,22 +12,25 @@ from google_file_downloader import (
 )
 
 class PacketDownloader:
-    def __init__(self, drive, folder_id, destination_dir):
+    def __init__(self, drive, folder_id, destination_dir, custom_save_filename_pattern):
         self.downloader = GoogleDriveFolderDownloader(drive)
         self.folder_id = folder_id
         self.file_type = FileTypeFilter(extensions=frozenset({"pdf"}))
         self.traversal_options = TraversalOptions(recursive=True, max_depth=-1)
-        self.download_options = DownloadOptions(
-            destination_dir = Path(destination_dir),
-            custom_filename=None,
-            duplicate_strategy = DuplicateFilenameStrategy.SKIP,
-        )
+        # self.download_options = DownloadOptions(
+        #     destination_dir = Path(destination_dir),
+        #     custom_filename=None,
+        #     duplicate_strategy = DuplicateFilenameStrategy.SKIP,
+        # )
+        self.destination_dir = Path(destination_dir)
+        self.custom_save_filename_pattern = custom_save_filename_pattern
+        self.duplicate_strategy = DuplicateFilenameStrategy.SKIP
         self.download_mode = DownloadMode.ALL
         self.case_sensitive = False
         self.match_mode = SearchMatchMode.EXACT
 
     def download_packet(self, id):
-        search_term = f"xx_{id}"
+        search_term = f"pa_{id}"
 
         search_options = SearchOptions(
             search_term=search_term,
@@ -35,10 +38,16 @@ class PacketDownloader:
             case_sensitive=self.case_sensitive,
         )
 
+        download_options = DownloadOptions(
+            destination_dir = self.destination_dir,
+            custom_filename=self.custom_save_filename_pattern.format(id=id),
+            duplicate_strategy = self.duplicate_strategy,
+        )
+
         result = self.downloader.download_matching_files(
             folder_id=self.folder_id,
             search=search_options,
-            download=self.download_options,
+            download=download_options,
             file_type=self.file_type,
             traversal=self.traversal_options,
             download_mode=self.download_mode,
